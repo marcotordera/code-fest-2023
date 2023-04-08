9dbfc5ee40e6ce9f4227937e8c849f62699ec59d
marcotordera <marcotordera@gmail.com>
2023-04-08 11:30:34 -0700

    added nav logo, axios and json server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { OnInit } from './Utilities/AppInit';
 import { TestComponent } from './components/TestComponent/TestComponent';
 import NavBar from './components/NavBar/NavBar';
 import LeftPanel from './components/LeftPanel/LeftPanel';
 import RightPanel from './components/RightPanel/RightPanel';
+import axios from 'axios';
 function App() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     const start = async () => {
       await OnInit();
+
+      await axios.get('http://localhost:3001/users').then((response) => {
+        setUsers(response.data);
+      });
     };
     start();
   });
   return (
     <div className="App">
       <NavBar />
-      <TestComponent />
       <LeftPanel>
         <h1>Panel Title</h1>
         <p>Panel content goes here</p>
@@ -24,6 +30,15 @@ function App() {
         <h1>Panel Title</h1>
         <p>Panel content goes here</p>
       </RightPanel>
+      <TestComponent />
+
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-
+import { AppBar, Toolbar, Typography, IconButton, Avatar } from '@material-ui/core';
+import { ExitToApp } from '@material-ui/icons';
+import avatar from '../../Assets/logo.png'
 const NavBar = observer(({ store }) => {
   const handleLogout = () => {
     console.log('test')
@@ -10,12 +11,13 @@ const NavBar = observer(({ store }) => {
   return (
     <AppBar position="static">
       <Toolbar>
+        <Avatar alt="Profile picture" src={avatar} style={{ marginRight: '10px', borderRadius: '50%',width: 50, height: 50 }} />
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           My App
         </Typography>
-        <Button color="inherit" onClick={handleLogout}>
-          Logout
-        </Button>
+        <IconButton color="inherit" onClick={handleLogout}>
+          <ExitToApp />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
